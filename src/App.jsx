3af9ad3c2493ae06d995bd243dc631c9a15fce27
thekristi <kristithecoder@gmail.com>
2026-08-03 16:57:46 -0700

    adding intial app and get Location util

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+      <div className="app-layout">
+        <div className="map-panel"></div>
+        <div className="admin-panel"></div>
+      </div>
+  )
+}
+
+export default App
